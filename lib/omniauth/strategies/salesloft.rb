a05511ecdd46d943245a40a7270c4e782209279a
Mike Sandt <michael.sandt@gmail.com>
2015-11-30 14:10:39 -0500

    Basic implementation of provider from wiki

--- lib/omniauth/strategies/salesloft.rb
+++ lib/omniauth/strategies/salesloft.rb
@@ -3,7 +3,22 @@ require "omniauth-oauth2"
 module OmniAuth
   module Strategies
     module Salesloft < OmniAuth::Strategies::OAuth2
-      # Your code goes here...
+      option :name, :salesloft
+
+      option :client_options, {
+        :site => "https://accounts.salesloft.com",
+        :authorize_url => "/oauth/authorize"
+      }
+
+      uid { raw_info["id"] }
+
+      info do
+        raw_info
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/api/v1/me.json")
+      end
     end
   end
 end
